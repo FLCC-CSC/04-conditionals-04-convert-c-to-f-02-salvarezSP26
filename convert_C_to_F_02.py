f10d56447be063fdc24cec220d0a73a030554282
Shanelle Alvarez <salvarez3@fingerlakes.edu>
2026-03-02 15:14:17 +0000

    Convert C to F pt2

--- convert_C_to_F_02.py
+++ convert_C_to_F_02.py
@@ -1,8 +1,8 @@
 # FILE NAME - convert_C_to_F_02.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Shanelle Alvarez
+# DATE: March 2, 2026
+# BRIEF DESCRIPTION: Celcius to Farenheight 2
 
 
 
@@ -12,16 +12,31 @@
 # 4. The Sample Output has been included in this code for your convenience
 
 
-
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+def main():
+  temp_convert()
 
+def temp_convert():
 
+  print('===== Temperature Converter =====')
+  print()
+  print("  1. Convert from Celsius to Fahrenheit     \n  2. Convert from Fahrenheit to Celsius ")
+  print()
+  choice = int(input('Please choose from the above menu: '))
+  temp = float(input('Enter a temperature to convert: '))
+  print()
 
+  
+  if (choice == 1):
+    c_to_f = float(temp *(9/5) + 32)
+    print (f'{temp} degrees Celsius is {c_to_f:.1f} degrees Fahrenheit.')
 
+  elif (choice == 2):
+    f_to_c = float((temp -32) * (5/9))
+    print(f'{temp} degrees Fahrenheit is {f_to_c:.1f} degrees Celsius.')
 
-
-
+main()    
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
@@ -93,7 +108,7 @@ Enter a temperature to convert: -40
 
 1. What is one lesson you learned in this lab?
 
-
+I learned how to properly use the float decimal place function that was mentioned in the practice lab.
 
 
 
